d55ed7a0d00eb48ffb1a534ce00937c5283776c0
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-09-08 14:53:38 -0400

    Adds dynamic route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 
     def home
+        @name = params[:name]
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  get '/home', to: 'application#home'
+  get '/home/:name', to: 'application#home'
 end
